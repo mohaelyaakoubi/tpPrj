2868043bc753e5a7dc691fd515fedd883367be91
harkouch <najat.harkouch@gmail.com>
2014-11-04 08:17:53 +0000

    premieres sources

---
+++ src/main/java/com/ensa/tpPrj/App.java
@@ -0,0 +1,14 @@
+package com.ensa.tpPrj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
